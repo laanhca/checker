c4f6b6618080ff3250384364b6e25c276b713ed6
laanhca <anhlv98@gmail.com>
2024-02-22 14:05:52 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='av-checker',
   packages=['av-checker'],
-  version='0.1.0',
+  version='0.1.1',
   license='MIT',
   description='check anything',
   author='Anh Van',
